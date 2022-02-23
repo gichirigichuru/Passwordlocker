9a40c2e0c06ed21a6a299eb305036fa0e31f34e8
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-23 20:40:36 +0300

    add setup method

--- test_account.py
+++ test_account.py
@@ -7,3 +7,20 @@ class TestAccount(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
   '''
+      # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_account = Account("James","Muri123",) # create contact object
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_account.user_name,"James")
+        self.assertEqual(self.new_account.password,"Muri123")
+if __name__ == '__main__':
+    unittest.main()
