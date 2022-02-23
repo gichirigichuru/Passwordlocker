a6db0914abd29ba95cd3f7e1a272834f6c53b11b
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-23 20:04:00 +0300

    add _init_method

--- account.py
+++ account.py
@@ -2,3 +2,7 @@ class Account:
   '''
   class that generates new instance of an account
   '''
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
+  
