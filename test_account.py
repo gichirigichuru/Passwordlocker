164c77aceb8f424c670df6f2575855902e8f57e3
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-23 20:20:34 +0300

    add testaccount class

---
+++ test_account.py
@@ -0,0 +1,9 @@
+import unittest # Importing the unittest module
+from account import Account #importing the account class
+class TestAccount(unittest.TestCase):
+    '''
+    Test class that defines test cases for the account class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+  '''
