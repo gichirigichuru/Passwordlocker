15c816f1c161b58e670204606879b5bcf7953b25
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-24 12:06:02 +0300

    func to create new account

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3.8
 from account import Account
+def create_account(uname,password):
+    '''
+    Function to create a new account
+    '''
+    new_account = Account(uname,password)
+    return new_account
